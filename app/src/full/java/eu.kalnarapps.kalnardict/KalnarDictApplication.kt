0a30057cedf4796f021677cd3ee00993403999d4
Robert KALNA <robert.kalna@ekino.com>
2020-07-09 15:24:46 +0200

    add stetho

--- app/src/full/java/eu.kalnarapps.kalnardict/KalnarDictApplication.kt
+++ app/src/full/java/eu.kalnarapps.kalnardict/KalnarDictApplication.kt
@@ -2,12 +2,12 @@ package eu.kalnarapps.kalnardict
 
 import android.app.Application
 import eu.kalnarapps.kalnardict.koin.dataModule
+import eu.kalnarapps.kalnardict.koin.mapperModule
 import eu.kalnarapps.kalnardict.koin.navigationKoinModule
 import eu.kalnarapps.kalnardict.koin.repositoryModule
 import eu.kalnarapps.kalnardict.koin.useCaseModule
 import eu.kalnarapps.kalnardict.koin.utilsModule
 import eu.kalnarapps.kalnardict.koin.viewModuleModule
-import eu.kalnarapps.kalnardict.koin.mapperModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
@@ -27,6 +27,10 @@ class KalnarDictApplication : Application() {
             modules(mapperModule)
             modules(viewModuleModule)
         }
+        //  uncomment if you want to use stetho
+        //  if (BuildConfig.DEBUG) {
+        //      Stetho.initializeWithDefaults(this)
+        //  }
     }
 
     override fun onTerminate() {
