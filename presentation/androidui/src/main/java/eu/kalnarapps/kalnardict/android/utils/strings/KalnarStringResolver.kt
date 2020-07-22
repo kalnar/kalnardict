dfe4c015cbb948ae3ec90cbff0e2124775ecdb25
Robert KALNA <robert.kalna@ekino.com>
2020-07-22 16:18:54 +0200

    add string resolver implementation

---
+++ presentation/androidui/src/main/java/eu/kalnarapps/kalnardict/android/utils/strings/KalnarStringResolver.kt
@@ -0,0 +1,30 @@
+package eu.kalnarapps.kalnardict.android.utils.strings
+
+import android.content.Context
+import eu.kalnarapps.kalnardict.androidui.R
+import eu.kalnarapps.kalnardict.models.strings.StringResolver
+import eu.kalnarapps.kalnardict.models.strings.StringResources
+
+class KalnarStringResolver(
+    private val context: Context
+) : StringResolver<StringResources.DictionaryQueryString> {
+    override fun lookUpString(resource: StringResources.DictionaryQueryString): String {
+        return when (resource) {
+            StringResources.DictionaryQueryString.QUERY_MODE_MATCH_ANYWHERE -> {
+                context.getString(R.string.query_mode_match_anywhere)
+            }
+            StringResources.DictionaryQueryString.QUERY_MODE_MATCH_BEGINNING -> {
+                context.getString(R.string.query_mode_match_beginning)
+            }
+            StringResources.DictionaryQueryString.QUERY_MODE_MATCH_END -> {
+                context.getString(R.string.query_mode_match_end)
+            }
+            StringResources.DictionaryQueryString.QUERY_MODE_MATCH_EXACT -> {
+                context.getString(R.string.query_mode_match_exact)
+            }
+            StringResources.DictionaryQueryString.QUERY_MODE_MATCH_FUZZY -> {
+                context.getString(R.string.query_mode_match_fuzzy)
+            }
+        }
+    }
+}
