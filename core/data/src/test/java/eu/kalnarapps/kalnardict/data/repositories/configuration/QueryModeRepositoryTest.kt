77afee8de95d9c240e1fd1c74f6366b99c2bb21b
Robert KALNA <robert.kalna@ekino.com>
2022-06-10 22:13:32 +0200

    [kal-42] update query mode repository test to use mocks

--- core/data/src/test/java/eu/kalnarapps/kalnardict/data/repositories/configuration/QueryModeRepositoryTest.kt
+++ core/data/src/test/java/eu/kalnarapps/kalnardict/data/repositories/configuration/QueryModeRepositoryTest.kt
@@ -1,13 +1,16 @@
 package eu.kalnarapps.kalnardict.data.repositories.configuration
 
-import eu.kalnarapps.kalnardict.data.mock.MockQueryModeDataSource
+import eu.kalnarapps.kalnardict.data.datasources.configuration.QueryModeConfigurationDataSource
 import eu.kalnarapps.kalnardict.data.test.TestCoroutineRule
 import eu.kalnarapps.kalnardict.data.test.test
 import eu.kalnarapps.kalnardict.domain.entities.dictionary.QueryMode
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verifyBlocking
 
 
 class QueryModeRepositoryTest {
@@ -15,14 +18,22 @@ class QueryModeRepositoryTest {
     @get:Rule
     val testCoroutineRule = TestCoroutineRule()
 
+    private val queryModeConfigurationDataSourceMock: QueryModeConfigurationDataSource = mock()
+
     @Test
-    fun initial_query_mode_match_beginning() {
+    fun when_fetching_query_mode_repository_then_get_query_mode_flow() {
         testCoroutineRule.runBlockingTest {
 
-            val repository =
-                QueryModeRepository(
-                    queryModeConfigurationDataSource = MockQueryModeDataSource()
-                )
+            `when`(
+                queryModeConfigurationDataSourceMock.getLastQueryModeId()
+            ).thenReturn(
+                flowOf(1)
+            )
+
+            val repository = QueryModeRepository(
+                queryModeConfigurationDataSource = queryModeConfigurationDataSourceMock
+            )
+
             repository.getCurrentQueryMode()
                 .test(scope = this)
                 .assertThatLastValue(
@@ -34,28 +45,19 @@ class QueryModeRepositoryTest {
     }
 
     @Test
-    fun update_current_dictionary() {
+    fun when_updating_query_mode_repository_then_call_update_query_mode_in_data_source() {
         testCoroutineRule.runBlockingTest {
-            val repository =
-                QueryModeRepository(
-                    queryModeConfigurationDataSource = MockQueryModeDataSource()
-                )
-            val testCollector = repository.getCurrentQueryMode()
-                .test(scope = this)
-                .assertThatLastValue(
-                    equalTo(QueryMode.MATCH_BEGINNING)
-                )
 
             val newQueryMode = QueryMode.MATCH_EXACT
 
+            val repository = QueryModeRepository(
+                queryModeConfigurationDataSource = queryModeConfigurationDataSourceMock
+            )
+
             repository.updateCurrentQueryMode(newQueryMode)
-
-            testCollector
-                .assertThatLastValue(
-                    equalTo(newQueryMode)
-                )
-                .finish()
-
+            verifyBlocking(queryModeConfigurationDataSourceMock) {
+                updateQueryMode(QueryMode.MATCH_EXACT.value)
+            }
 
         }
     }
