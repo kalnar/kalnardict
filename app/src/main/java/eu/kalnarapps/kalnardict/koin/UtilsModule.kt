cf213becf1597995bb9561a8399a0ebd2e6fc6cd
Robert KALNA <robert.kalna@ekino.com>
2022-06-14 19:57:24 +0200

    [kal-46] update UriAdapter to prepend with storage root if uri is relative

--- app/src/main/java/eu/kalnarapps/kalnardict/koin/UtilsModule.kt
+++ app/src/main/java/eu/kalnarapps/kalnardict/koin/UtilsModule.kt
@@ -3,11 +3,12 @@ package eu.kalnarapps.kalnardict.koin
 import eu.kalnarapps.kalnardict.android.utils.KalnarUiLogger
 import eu.kalnarapps.kalnardict.presentation.interactors.errorhandlers.UiLogger
 import eu.kalnarapps.kalnardict.android.utils.uri.UriAdapter
+import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 
 val utilsModule: Module = module {
     single { KalnarUiLogger() as UiLogger }
-    single { UriAdapter() }
+    single { UriAdapter(androidApplication()) }
 }
--- presentation/androidui/src/main/java/eu/kalnarapps/kalnardict/android/utils/uri/UriAdapter.kt
+++ presentation/androidui/src/main/java/eu/kalnarapps/kalnardict/android/utils/uri/UriAdapter.kt
@@ -1,9 +1,20 @@
 package eu.kalnarapps.kalnardict.android.utils.uri
 
+import android.content.Context
 import android.net.Uri
+import eu.kalnarapps.kalnardict.common.utils.getStorageRootPath
+import java.io.File
 
-class UriAdapter {
+class UriAdapter(
+    private val context: Context
+) {
     fun convertUriToSdcardPath(uri: Uri?): String {
-        return uri?.lastPathSegment?.split(":")?.get(1).orEmpty()
+        val path = uri?.lastPathSegment?.split(":")?.get(1).orEmpty()
+        val file = File(path)
+        return if (file.exists()) {
+            path
+        } else {
+            "${context.getStorageRootPath()}/$path"
+        }
     }
 }
