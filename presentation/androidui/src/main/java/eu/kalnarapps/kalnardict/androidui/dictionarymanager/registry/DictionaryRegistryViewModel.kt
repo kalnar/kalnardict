c2b6f7a5467e2fb746176a942ff7b43927595f2e
Robert KALNA <robert.kalna@ekino.com>
2022-06-14 20:01:17 +0200

    [kal-46] convert LoadableContent.Failed to data class

--- presentation/androidui/src/main/java/eu/kalnarapps/kalnardict/androidui/dictionarymanager/registry/DictionaryRegistryViewModel.kt
+++ presentation/androidui/src/main/java/eu/kalnarapps/kalnardict/androidui/dictionarymanager/registry/DictionaryRegistryViewModel.kt
@@ -76,7 +76,7 @@ class DictionaryRegistryViewModel(
                                     )
                                 )
                             )
-                            LoadableContent.Failed
+                            LoadableContent.Failed(metaInfoFetch)
                         }
                     },
                     availableLanguages = listAvailableLanguages()
