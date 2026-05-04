51f743d8e32ab253f12867df6fe31e91b3e1042d
Robert KALNA <robert.kalna.dev@gmail.com>
2026-05-04 15:44:09 +0200

    fix configurationPropertyDao.updateProperty function

--- localdata/src/main/java/eu/kalnarapps/kalnardict/data/dao/configuration/ConfigurationPropertyDao.kt
+++ localdata/src/main/java/eu/kalnarapps/kalnardict/data/dao/configuration/ConfigurationPropertyDao.kt
@@ -2,6 +2,7 @@ package eu.kalnarapps.kalnardict.data.dao.configuration
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import eu.kalnarapps.kalnardict.data.entities.ConfigurationProperty
@@ -33,7 +34,7 @@ interface ConfigurationPropertyDao {
     // @formatter:on
     fun getFlowPropertyByKey(key: String): Flow<ConfigurationProperty>
 
-    @Update
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun updateProperty(property: ConfigurationProperty)
 
     @Insert
